426eb084a7e9980e391432d8ea378f29dabfcbce
evhenii.alieksnadrov <yalieksnadrov@solvexus.com>
2025-04-21 21:16:19 +0300

    #01 feat: coming-soon

---
+++ config/middlewares.ts
@@ -0,0 +1,86 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          upgradeInsecureRequests: null,
+          'connect-src': [
+            "'self'",
+            'https:',
+            'http:',
+            'api.telegram.org',
+            'api.liqpay.ua'
+          ],
+          'frame-src': [
+            "'self'",
+            'editor.unlayer.com',
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'editor.unlayer.com',
+            'maps.googleapis.com',
+          
+          ],
+          'script-src-elem': [
+            "'self'",
+            "'unsafe-inline'",
+            'editor.unlayer.com',
+            'maps.googleapis.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com',
+            'maps.gstatic.com',
+            'maps.googleapis.com'
+          ],
+          'img-src': [
+            "'self'",
+            'blob:',
+            'data:',
+            'strapi.io',
+            'cdn.jsdelivr.net',
+            's3.amazonaws.com',
+            'res.cloudinary.com',
+            'market-assets.strapi.io',
+            'khmdb0.google.com',
+            'khmdb0.googleapis.com',
+            'khmdb1.google.com',
+            'khmdb1.googleapis.com',
+            'khm.google.com',
+            'khm.googleapis.com',
+            'khm0.google.com',
+            'khm0.googleapis.com',
+            'khm1.google.com',
+            'khm1.googleapis.com',
+            'khms0.google.com',
+            'khms0.googleapis.com',
+            'khms1.google.com',
+            'khms1.googleapis.com',
+            'khms2.google.com',
+            'khms2.googleapis.com',
+            'khms3.google.com',
+            'khms3.googleapis.com',
+            'streetviewpixels-pa.googleapis.com',
+          ],
+          'form-action': [
+            "'self'",
+          ],
+        },
+      },
+    },
+  },
+];
---
+++ types/generated/components.d.ts
@@ -0,0 +1,76 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    description: '';
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    coverXl: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    coverXs: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+  };
+}
+
+export interface SharedOpenGraph extends Struct.ComponentSchema {
+  collectionName: 'components_shared_open_graphs';
+  info: {
+    displayName: 'openGraph';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    ogDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 200;
+      }>;
+    ogImage: Schema.Attribute.Media<'images'>;
+    ogTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 70;
+      }>;
+    ogType: Schema.Attribute.String;
+    ogUrl: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    canonicalURL: Schema.Attribute.String;
+    keywords: Schema.Attribute.Text;
+    metaDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+        minLength: 50;
+      }>;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaRobots: Schema.Attribute.String;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaViewport: Schema.Attribute.String;
+    openGraph: Schema.Attribute.Component<'shared.open-graph', false>;
+    structuredData: Schema.Attribute.JSON;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.media': SharedMedia;
+      'shared.open-graph': SharedOpenGraph;
+      'shared.seo': SharedSeo;
+    }
+  }
+}
